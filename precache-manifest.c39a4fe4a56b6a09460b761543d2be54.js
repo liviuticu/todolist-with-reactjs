42a373682d198490b1d6d87c8bcd198724a811d3
Liviu Ticu <liviuticu@yahoo.com>
2023-02-22 20:46:35 +0200

    Updates

---
+++ precache-manifest.c39a4fe4a56b6a09460b761543d2be54.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "9b4e22e7b1d5ab11923b",
+    "url": "/todolist-with-reactjs/static/js/main.9b4e22e7.chunk.js"
+  },
+  {
+    "revision": "aa3d6f7b9b294bf4deba",
+    "url": "/todolist-with-reactjs/static/js/1.aa3d6f7b.chunk.js"
+  },
+  {
+    "revision": "0c402ac51095dd4b0051",
+    "url": "/todolist-with-reactjs/static/js/runtime~main.0c402ac5.js"
+  },
+  {
+    "revision": "9baf786fb572fdb44750626c950e0f7b",
+    "url": "/todolist-with-reactjs/index.html"
+  }
+];
